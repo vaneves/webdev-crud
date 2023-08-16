fdf1b5b16f9f85c4d1232b04fa4f1ef7b9f10b4d
Van Neves <vaneves@vaneves.com>
2023-08-16 20:27:56 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [publicacoes, setPublicacoes] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((resposta) => {
+        setPublicacoes(resposta.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {publicacoes.map((publicacao) => {
+          return <li>{publicacao.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
